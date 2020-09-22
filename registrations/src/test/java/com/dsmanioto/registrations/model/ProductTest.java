344fd0b56c4e671c138ce367e76754f4eb99a508
Daniel Smanioto <daniel.smanioto@gmail.com>
2020-09-21 22:35:38 -0300

    Products Model test

---
+++ registrations/src/test/java/com/dsmanioto/registrations/model/ProductTest.java
@@ -0,0 +1,23 @@
+package com.dsmanioto.registrations.model;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+
+public class ProductTest {
+
+    @Test
+    public void validateFields() {
+        Product product = Product.builder()
+                .id(1L)
+                .name("Coca cola 2l")
+                .price(BigDecimal.valueOf(2.98))
+                .build();
+
+        Assertions.assertEquals(1L, product.getId() );
+        Assertions.assertEquals("Coca cola 2l", product.getName());
+        Assertions.assertEquals(BigDecimal.valueOf(2.98), product.getPrice());
+    }
+
+}
