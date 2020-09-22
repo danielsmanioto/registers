cd82764413a669eec6b0461221cc899468fff924
Daniel Smanioto <daniel.smanioto@gmail.com>
2020-09-21 22:30:23 -0300

    CustomerModel teste

---
+++ registrations/src/test/java/com/dsmanioto/registrations/model/CustomerTest.java
@@ -0,0 +1,21 @@
+package com.dsmanioto.registrations.model;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class CustomerTest {
+
+    @Test
+    public void validateFields() {
+        Customer customer = Customer.builder()
+                .id(1L)
+                .name("Daniel")
+                .email("daniel@alksjdklajsdqe.com")
+                .build();
+
+        Assertions.assertEquals(1L, customer.getId());
+        Assertions.assertEquals("Daniel", customer.getName());
+        Assertions.assertEquals("daniel@alksjdklajsdqe.com", customer.getEmail());
+    }
+
+}
