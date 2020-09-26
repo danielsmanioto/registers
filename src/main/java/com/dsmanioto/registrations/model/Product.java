0c0c8f6b1a33a8e7e787aaf699bdc915f24f9389
Daniel Smanioto <daniel.smanioto@gmail.com>
2020-09-25 22:13:10 -0300

    identity

--- src/main/java/com/dsmanioto/registrations/model/Product.java
+++ src/main/java/com/dsmanioto/registrations/model/Product.java
@@ -20,7 +20,7 @@ import java.math.BigDecimal;
 public class Product {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String name;
