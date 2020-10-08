2205b6bdcee15b42caa0e98186398c2b0130f9f1
Daniel Smanioto <daniel.smanioto@gmail.com>
2020-10-07 21:33:59 -0300

    Organizando bibiliotecas w configurando wildcard *

--- src/main/java/com/dsmanioto/registrations/model/UserReg.java
+++ src/main/java/com/dsmanioto/registrations/model/UserReg.java
@@ -1,7 +1,12 @@
 package com.dsmanioto.registrations.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 import org.dom4j.tree.AbstractEntity;
 
 import javax.persistence.Column;
